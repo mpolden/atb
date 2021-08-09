71c25120e387f6c1c384172c85dad88d7e9c8880
Martin Polden <mpolden@mpolden.no>
2021-08-09 13:17:01 +0200

    build: Stop ignoring SA5008

--- tools.go
+++ tools.go
@@ -1,7 +1,6 @@
 // +build tools
 
-// Disable SA5008 because cmd packages has a duplicate "choice" tag
-//go:generate go run honnef.co/go/tools/cmd/staticcheck -checks inherit,-SA5008 ./...
+//go:generate go run honnef.co/go/tools/cmd/staticcheck -checks inherit ./...
 
 package tools
 
